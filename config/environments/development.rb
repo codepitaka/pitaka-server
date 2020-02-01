cce5bde5d2f0a4c9859f85f980b08ff6e77187d8
chullino <chullino93@gmail.com>
2020-02-01 13:36:40 +0000

    allow web-console in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   require_relative 'available_hosts'
-  # config.web_console.whiny_requests = false
+  config.web_console.whiny_requests = false
   # Do not eager load code on boot.
   config.eager_load = false
   # Show full error reports.
