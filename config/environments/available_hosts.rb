0ed01d3b53ed74481ec01e8e89213f4957b81fc2
chullino <chullino93@gmail.com>
2020-02-01 13:56:04 +0000

    linter auto correct

--- config/environments/available_hosts.rb
+++ config/environments/available_hosts.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   config.hosts.push 'port-3000-pitaka.run.goorm.io', 'pitaka-jnclz.run.goorm.io',
                     'pitaka-web-page-edit-we5mit4np.herokuapp.com', 'pitaka-web-dev.herokuapp.com',
-                    'pitaka-web.herokuapp.com','pitaka-serve-publish-ap-qc2c9q.herokuapp.com',
-					'pitaka-server-dev.herokuapp.com',
-					'pitaka-server.herokuapp.com'
+                    'pitaka-web.herokuapp.com', 'pitaka-serve-publish-ap-qc2c9q.herokuapp.com',
+                    'pitaka-server-dev.herokuapp.com',
+                    'pitaka-server.herokuapp.com'
 end
