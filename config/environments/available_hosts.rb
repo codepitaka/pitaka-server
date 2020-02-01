e660947f9e72e3816e0a5134999bfa845292ad11
chullino <chullino93@gmail.com>
2020-02-01 13:23:18 +0000

    remove faulty comma

--- config/environments/available_hosts.rb
+++ config/environments/available_hosts.rb
@@ -3,6 +3,6 @@
 Rails.application.configure do
   config.hosts.push 'port-3000-pitaka.run.goorm.io', 'pitaka-jnclz.run.goorm.io',
                     'pitaka-web-page-edit-we5mit4np.herokuapp.com', 'pitaka-web-dev.herokuapp.com',
-                    'pitaka-web.herokuapp.com',
-                    config.web_console.whiny_requests = false
+                    'pitaka-web.herokuapp.com'
+  config.web_console.whiny_requests = false
 end
