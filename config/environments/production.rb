cf78d6c60f3ab42210bad6a0ee74008c4fc33ef6
chullino <chullino93@gmail.com>
2020-02-01 11:19:46 +0000

    add modularized available hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.hosts.push 'pitaka-web.herokuapp.com'
+  config.web_console.whiny_requests = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
