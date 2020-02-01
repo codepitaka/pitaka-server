5bf2b11ab0d067257b2c01bcfa77f1180f9073d5
chullino <chullino93@gmail.com>
2020-02-01 12:15:30 +0000

    add identical available_hosts to prd, test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts.push 'pitaka-web.herokuapp.com'
-  config.web_console.whiny_requests = false
+  require_relative 'available_hosts'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
