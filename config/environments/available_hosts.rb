1c547141edfc8dee9e970134b2af4649f1d8a852
chullino <chullino93@gmail.com>
2020-02-01 13:25:07 +0000

    refactor web_console

--- config/environments/available_hosts.rb
+++ config/environments/available_hosts.rb
@@ -4,5 +4,4 @@ Rails.application.configure do
   config.hosts.push 'port-3000-pitaka.run.goorm.io', 'pitaka-jnclz.run.goorm.io',
                     'pitaka-web-page-edit-we5mit4np.herokuapp.com', 'pitaka-web-dev.herokuapp.com',
                     'pitaka-web.herokuapp.com'
-  config.web_console.whiny_requests = false
 end
