e1e37cfe8c1d0c04c5b9b9f93c544dad11db58fa
chullino <chullino93@gmail.com>
2020-02-01 12:15:30 +0000

    add identical available_hosts to prd, test

--- config/environments/available_hosts.rb
+++ config/environments/available_hosts.rb
@@ -2,6 +2,7 @@
 
 Rails.application.configure do
   config.hosts.push 'port-3000-pitaka.run.goorm.io', 'pitaka-jnclz.run.goorm.io',
-                    'pitaka-web-page-edit-we5mit4np.herokuapp.com', 'pitaka-web-dev.herokuapp.com'
-  config.web_console.whiny_requests = false
+                    'pitaka-web-page-edit-we5mit4np.herokuapp.com', 'pitaka-web-dev.herokuapp.com',
+                    'pitaka-web.herokuapp.com',
+                    config.web_console.whiny_requests = false
 end
