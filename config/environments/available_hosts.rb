1df25ffbccae49e9ea40ce7c559fe8b1487db0bd
chullino <chullino93@gmail.com>
2020-02-02 02:39:10 +0000

    set permittedHosts for each environment

--- config/environments/available_hosts.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.configure do
-  config.hosts.push 'port-3000-pitaka.run.goorm.io', 'pitaka-jnclz.run.goorm.io',
-                    'pitaka-web-page-edit-we5mit4np.herokuapp.com', 'pitaka-web-dev.herokuapp.com',
-                    'pitaka-web.herokuapp.com', 'pitaka-serve-publish-ap-qc2c9q.herokuapp.com',
-                    'pitaka-server-dev.herokuapp.com',
-                    'pitaka-server.herokuapp.com'
-end
