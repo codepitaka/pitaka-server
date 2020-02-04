1ae9b8b76ca8e998fa06123cf19d18994112b44d
chullino <chullino93@gmail.com>
2020-02-04 11:59:24 +0000

    clean dirts from code - revert trailing slash. - revert response status to be 200

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,14 +5,14 @@
 require 'rails_helper'
 
 describe 'APIs related with posts', type: :request do
-  it 'GET /posts' do
+  it 'GET /posts/' do
     blog = Blog.create(title: 'test blog')
     Post.create(title: 'test post 1', subtitle: 'subtitle1', content: 'c1', blog_id: blog.id)
     Post.create(title: 'test post 2', subtitle: 'subtitle2', content: 'c2', blog_id: blog.id)
 
-    get '/posts'
+    get '/posts/'
 
-    # expect(response.status).to be(200)
+    expect(response.status).to be(200)
     expect(JSON.parse(response.body)['data'].size).to eq(2)
     expect(JSON.parse(response.body)['data'][1]['title']).to eq('test post 2')
   end
@@ -24,7 +24,7 @@ describe 'APIs related with posts', type: :request do
 
     get '/posts/' + post.id.to_s
 
-    # expect(response.status).to be(200)
+    expect(response.status).to be(200)
     expect(JSON.parse(response.body)['data']['title']).to eq(post.title)
     expect(JSON.parse(response.body)['data']['subtitle']).to eq(post.subtitle)
   end
