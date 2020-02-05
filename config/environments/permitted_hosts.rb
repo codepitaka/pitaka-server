692120b40472341c17c118f4d26b6706f9067d16
chullino <chullino93@gmail.com>
2020-02-05 05:37:01 +0000

    resolve linter

--- config/environments/permitted_hosts.rb
+++ config/environments/permitted_hosts.rb
@@ -5,8 +5,7 @@ module PermittedHosts
   def self.development
     Rails.application.configure do
       # local/heroku's pitaka-server develop environment hosts
-	  if !ENV['PITAKA_SERVER_HOST']=='':
-        config.hosts.push ENV['PITAKA_SERVER_HOST']
+      config.hosts.push ENV['PITAKA_SERVER_HOST']
       # heroku's pitaka review app hosts
       if !ENV['HEROKU_PR_NUMBER'] == ''
         review_app_name = ENV['HEROKU_APP_NAME']
@@ -19,7 +18,6 @@ module PermittedHosts
   def self.production
     Rails.application.configure do
       # heroku's pitaka-server production environment hosts
-	  if !ENV['PITAKA_SERVER_HOST']=='':
       config.hosts.push ENV['PITAKA_SERVER_HOST']
     end
   end
