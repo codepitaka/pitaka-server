9e30f5624d05b84c8b722e6b7cc59c8a47023f21
chullino <chullino93@gmail.com>
2020-02-09 04:18:10 +0000

    remove unnecessary comments

--- config/application.rb
+++ config/application.rb
@@ -13,15 +13,10 @@ module Pitaka
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    # config.action_dispatch.default_headers = {
-    #   'Access-Control-Allow-Origin' => '*',
-    #   'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
-    #   'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept'
-    # }
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
 	    origins Rails.application.credentials[Rails.env.to_sym][:allowed_origins]
-		# origins '*'
         resource '*',
     	headers: ['Content-Type'],
 		credentials: false,
